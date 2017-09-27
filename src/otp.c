3dc528121ef5764a2485db14a923af90f9f886fb
Mr. Teeminus <32164856+teeminus@users.noreply.github.com>
2017-09-27 14:39:07 +0200

    - Fixed building of OpenSSL 1.1

--- src/otp.c
+++ src/otp.c
@@ -395,13 +395,14 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
             LOG("OTP-AUTH: unknown encoding '%s'\n", otp_params.encoding);
             goto done;
         }
-    
+
         uint64_t T, Tn, Ti;
         uint8_t mac[EVP_MAX_MD_SIZE];
         unsigned maclen;
 
+
         if (!strncasecmp("totp", otp_params.method, 4)) {
-            HMAC_CTX hmac;
+            HMAC_CTX *hmac = HMAC_CTX_new ();
             const uint8_t *otp_bytes;
             uint32_t otp, divisor = 1;
             int tstep = totp_step;
@@ -422,10 +423,10 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
             for (i = -range; !ok && i <= range; ++i) {
                 Tn = htobe64(T + i);
 
-                HMAC_CTX_init(&hmac);
-                HMAC_Init(&hmac, otp_key, key_len, otp_digest);
-                HMAC_Update(&hmac, (uint8_t *)&Tn, sizeof(Tn));
-                HMAC_Final(&hmac, mac, &maclen);
+                HMAC_CTX_reset(hmac);
+                HMAC_Init_ex(hmac, otp_key, key_len, otp_digest, NULL);
+                HMAC_Update(hmac, (uint8_t *)&Tn, sizeof(Tn));
+                HMAC_Final(hmac, mac, &maclen);
 
                 otp_bytes = mac + (mac[maclen - 1] & 0x0f);
                 otp = ((otp_bytes[0] & 0x7f) << 24) | (otp_bytes[1] << 16) |
@@ -442,9 +443,10 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
                     DEBUG("OTP-AUTH: auth ok for method='%s', client_username='%s', client_secret='%s'\n", otp_params.method, vpn_username, vpn_secret);
                 }
             }
+            HMAC_CTX_free (hmac);
         }
         else if (!strncasecmp("hotp", otp_params.method, 4)) {
-            HMAC_CTX hmac;
+            HMAC_CTX *hmac = HMAC_CTX_new();
             const uint8_t *otp_bytes;
             uint32_t otp, divisor = 1;
             int tdigits = totp_digits;
@@ -463,10 +465,10 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
                   Ti = T+i;
                   Tn = htobe64(Ti);
 
-                  HMAC_CTX_init(&hmac);
-                  HMAC_Init(&hmac, otp_key, key_len, otp_digest);
-                  HMAC_Update(&hmac, (uint8_t *)&Tn, sizeof(Tn));
-                  HMAC_Final(&hmac, mac, &maclen);
+                  HMAC_CTX_reset(hmac);
+                  HMAC_Init_ex(hmac, otp_key, key_len, otp_digest, NULL);
+                  HMAC_Update(hmac, (uint8_t *)&Tn, sizeof(Tn));
+                  HMAC_Final(hmac, mac, &maclen);
 
                   otp_bytes = mac + (mac[maclen - 1] & 0x0f);
                   otp = ((otp_bytes[0] & 0x7f) << 24) | (otp_bytes[1] << 16) |
@@ -485,6 +487,7 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
                   }
               }
             }
+            HMAC_CTX_free (hmac);
         }
         else if (!strcasecmp("motp", otp_params.method)) {
             char buf[64];
@@ -494,19 +497,19 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
             T = time(NULL) / motp_step;
 
             for (i = -range; !ok && i <= range; ++i) {
-                EVP_MD_CTX ctx;
-                EVP_MD_CTX_init(&ctx);
-                EVP_DigestInit_ex(&ctx, otp_digest, NULL);
+                EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+                EVP_MD_CTX_init(ctx);
+                EVP_DigestInit_ex(ctx, otp_digest, NULL);
                 n = sprintf(buf, "%" PRIu64, T + i);
-                EVP_DigestUpdate(&ctx, buf, n);
-                EVP_DigestUpdate(&ctx, otp_key, key_len);
-                EVP_DigestUpdate(&ctx, otp_params.pin, strlen(otp_params.pin));
+                EVP_DigestUpdate(ctx, buf, n);
+                EVP_DigestUpdate(ctx, otp_key, key_len);
+                EVP_DigestUpdate(ctx, otp_params.pin, strlen(otp_params.pin));
                 if (otp_params.udid) {
                     int udid_len = strlen(otp_params.udid);
-                    EVP_DigestUpdate(&ctx, otp_params.udid, udid_len);
+                    EVP_DigestUpdate(ctx, otp_params.udid, udid_len);
                 }
-                EVP_DigestFinal_ex(&ctx, mac, &maclen);
-                EVP_MD_CTX_cleanup(&ctx);
+                EVP_DigestFinal_ex(ctx, mac, &maclen);
+                EVP_MD_CTX_cleanup(ctx);
 
                 snprintf(secret, sizeof(secret),
                          "%02x%02x%02x", mac[0], mac[1], mac[2]);
@@ -518,6 +521,7 @@ static int otp_verify(const char *vpn_username, const char *vpn_secret)
                     ok = 1;
                     DEBUG("OTP-AUTH: auth ok for method='%s', client_username='%s', client_secret='%s'\n", otp_params.method, vpn_username, vpn_secret);
                 }
+                EVP_MD_CTX_free (ctx);
             }
         }
         else {
@@ -656,7 +660,8 @@ openvpn_plugin_func_v1 (openvpn_plugin_handle_t handle, const int type, const ch
   const int ulen = strlen(username);
   const int pwlen = strlen(password);
   if ( ulen > MAXWORDLEN || ulen == 0 || pwlen > MAXWORDLEN || pwlen == 0) {
-	  return OPENVPN_PLUGIN_FUNC_ERROR;
+    LOG("OTP-AUTH: String length check failed => username (%d), password(%d)\n", ulen, pwlen);
+    return OPENVPN_PLUGIN_FUNC_ERROR;
   }
 
   /* check entered username/password against what we require */
